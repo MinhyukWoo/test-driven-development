bc218aa840ab2c32f83164bb16fc60f002638e12
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-12-25 22:40:45 +0900

    Add moneyFactory to remove concrete class

--- part01/chapter08/test/application.test.js
+++ part01/chapter08/test/application.test.js
@@ -1,30 +1,30 @@
-import { Dollar } from '../src/Dollar';
-import { Franc } from '../src/franc';
+import { MoneyFactory } from '../src/moneyFactory';
+import { Money } from '../src/money';
 
 describe('Test of Currency', () => {
   test('Test Dollar Multipliaction', () => {
-    const five = new Dollar(5);
-    expect(five).toBeInstanceOf(Dollar);
+    const five = MoneyFactory.dollar(5);
+    expect(five).toBeInstanceOf(Money);
 
-    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(2).equals(MoneyFactory.dollar(10))).toBeTruthy();
 
-    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
+    expect(five.times(3).equals(MoneyFactory.dollar(15))).toBeTruthy();
   });
 
   test('Test Equality', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
-    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
-    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
+    expect(MoneyFactory.dollar(5).equals(MoneyFactory.dollar(5))).toBeTruthy();
+    expect(MoneyFactory.dollar(5).equals(MoneyFactory.dollar(6))).toBeFalsy();
+    expect(MoneyFactory.franc(5).equals(MoneyFactory.franc(5))).toBeTruthy();
+    expect(MoneyFactory.franc(5).equals(MoneyFactory.franc(6))).toBeFalsy();
+    expect(MoneyFactory.franc(5).equals(MoneyFactory.dollar(5))).toBeFalsy();
   });
 
   test('Test Franc Multipliaction', () => {
-    const five = new Franc(5);
-    expect(five).toBeInstanceOf(Franc);
+    const five = MoneyFactory.franc(5);
+    expect(five).toBeInstanceOf(Money);
 
-    expect(five.times(2).equals(new Franc(10))).toBeTruthy();
+    expect(five.times(2).equals(MoneyFactory.franc(10))).toBeTruthy();
 
-    expect(five.times(3).equals(new Franc(15))).toBeTruthy();
+    expect(five.times(3).equals(MoneyFactory.franc(15))).toBeTruthy();
   });
 });
