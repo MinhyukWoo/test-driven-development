1ab003aaee9439c101e277b9e947c37308d46d45
MinhyukWoo <minhyukwoo.dev@gmail.com>
2024-12-25 13:04:28 +0900

    Change times interface for chapter02

--- part01/chapter02/test/application.test.js
+++ part01/chapter02/test/application.test.js
@@ -2,6 +2,12 @@ import { Dollar } from '../src/Dollar';
 
 test('test multipliaction', () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toBe(10);
+  expect(five).toBeInstanceOf(Dollar);
+  
+  let product = five.times(2);
+  expect(product).toBeInstanceOf(Dollar);
+  expect(product.amount).toBe(10);
+
+  product = five.times(3);
+  expect(product.amount).toBe(15);
 });
